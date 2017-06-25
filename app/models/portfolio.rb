536ce53ac82084d5033c9caaffcb2f427b550749
Jordan Hudgens <jordan@devcamp.com>
2017-06-25 18:59:30 +0000

    Added nested attributes to new portfolio creation

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -16,9 +16,5 @@ class Portfolio < ApplicationRecord
    def set_defaults
        self.main_image ||=  Placeholder.image_generator(height: '600', width: '400')
        self.thumb_image ||=  Placeholder.image_generator(height: '350', width: '200')
-   end   
-end
-
-if self.main_image == nil
-    self.main_image = "http://placehold.it/600x400"
+   end  
 end
