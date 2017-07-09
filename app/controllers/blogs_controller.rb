5e71a092364826b17547f400cf76f2cd5b039b24
Jordan Hudgens <jordan@devcamp.com>
2017-07-09 20:20:41 +0000

    added puts debugging

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -4,8 +4,10 @@ class BlogsController < ApplicationController
   # GET /blogs
   # GET /blogs.json
   def index
-    @blogs = Blog.all
+    @blogs = Blog.special_blogs
+    puts "*" * 500
     @page_title = "My Portfolio Blog"
+    puts "*" * 500
   end
 
   # GET /blogs/1
