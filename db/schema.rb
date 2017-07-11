91f2716be22917f936d6828832c49e86379e8265
Jordan Hudgens <jordan@devcamp.com>
2017-07-11 15:38:05 +0000

    Fixed some bugs and finished petergate implementation

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20170626143205) do
-=======
-ActiveRecord::Schema.define(version: 20170629190524) do
->>>>>>> rubygem
+ActiveRecord::Schema.define(version: 20170711153352) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,6 +68,7 @@ ActiveRecord::Schema.define(version: 20170629190524) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "roles"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
