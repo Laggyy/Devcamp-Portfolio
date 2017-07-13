9b8125c660c03d9bd8f0ed07c43c06ed459443d5
Jordan Hudgens <jordan@devcamp.com>
2017-07-13 14:43:09 +0000

    added styles for register page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,8 +6,6 @@ module ApplicationHelper
       (link_to "Login", new_user_session_path, class: style)
     else
       (link_to "Logout", destroy_user_session_path, method: :delete, class: style)
-      " ".html_safe +
-      (link_to "Edit", edit_user_registration_path, class: style)
     end
   end
 
